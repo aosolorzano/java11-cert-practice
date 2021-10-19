7b700737de05a6cbd631ec9bf2d0567ae5787449
Andres Solorzano <asolorzano@technisys.com>
2021-10-18 20:25:51 -0500

    Initial practice test class for chapter 12

---
+++ chapters/src/test/java/com/hiperium/java/cert/practice/tests/Chapter12Test.java
@@ -0,0 +1,11 @@
+package com.hiperium.java.cert.practice.tests;
+
+/**
+ * THE OCP EXAM TOPICS COVERED IN THIS PRACTICE TEST INCLUDE THE FOLLOWING:
+ *
+ *      * Localization
+ *          - Implement Localization using Locale, resource bundles, and Java APIs to parse and format messages, dates,
+ *            and numbers.
+ */
+public class Chapter12Test {
+}
